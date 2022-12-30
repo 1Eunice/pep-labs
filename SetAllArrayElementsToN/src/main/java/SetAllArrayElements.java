6a0f4bf09fe12d9b6080006a6749f50b68ac7ccf
1Eunice <1eunicegill@gmail.com>
2022-12-30 19:16:10 +0000

    Saved progress on SetAllArrayElementsToN 2022-12-30 19:16:10 +0000

--- SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
+++ SetAllArrayElementsToN/src/main/java/SetAllArrayElements.java
@@ -26,9 +26,8 @@ public class SetAllArrayElements {
     public int set(int[] arr, int n){
 
         for (int i = 0; i < arr.length; i++) {
-            if(arr[i] > arr.length);
-                  
+            arr[i] = n;  
         }
-        return arr[n];
+        return n;
     }   
 }
