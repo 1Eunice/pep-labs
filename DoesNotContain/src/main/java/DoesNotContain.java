14e4b94669a9f50eb4f47af1e603c56ced3f3bc3
1Eunice <1eunicegill@gmail.com>
2023-01-12 22:52:46 +0000

    Saved progress on DoesNotContain 2023-01-12 22:52:46 +0000

--- DoesNotContain/src/main/java/DoesNotContain.java
+++ DoesNotContain/src/main/java/DoesNotContain.java
@@ -13,11 +13,7 @@ public class DoesNotContain {
      * @return true if arr does not contain target. false otherwise.
      */
     public boolean arrayDoesNotContain(int[] arr, int target){
-        for(int c : arr){
-            if(c.DoesNotContain(target)){
-                return true;
-            }
-        }
+        
         return false;
        
     }
