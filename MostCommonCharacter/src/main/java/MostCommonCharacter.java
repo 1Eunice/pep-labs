6e7ef0e1fc9265ce5ab693f60d40b622746287dd
1Eunice <1eunicegill@gmail.com>
2023-02-07 23:53:21 +0000

    Saved progress on MostCommonCharacter 2023-02-07 23:53:21 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -1,3 +1,9 @@
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javafx.util.Pair;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +14,48 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        Pair<Character, Integer> maxCharCountPair = getMostRepeatedCharacterFromString(str);
+        maxCharCountPair.getKey();
+        maxCharCountPair.getValue();
+        return 0;
+    }
+
+    public static Pair<Character, Integer> getMostRepeatedCharacterFromString(String str) {
+        Map<Character, Integer> countMap = new HashMap<>();
+        char[] chars = str.toCharArray();
+        for (char ch : chars) {
+            if (!Character.isWhitespace(ch)) {
+                Integer currentCount = countMap.get(ch);
+                if (currentCount == null) {
+                    countMap.put(ch, 1);
+                } else {
+                    countMap.put(ch, ++currentCount);
+                }
+            }
+        }
+        Integer maxCharacterCount = Collections.max(countMap.values());
+        char maxCharacter = Character.MIN_VALUE;
+        for (Entry<Character, Integer> entry : countMap.entrySet()) {
+            maxCharacter = entry.getKey();
+            return Pair(str, maxCharacterCount);
+        }
+    
+        return Pair(str, maxCharacter);
+    }
+
+    private static javafx.util.Pair<Character, Integer> Pair(String str, Integer maxCharacterCount) {
+        return null;
+    }
+
+    private static javafx.util.Pair<Character, Integer> Pair(String str, char maxCharacter) {
+        return null;
+    }
+
+    public static javafx.util.Pair<Character, Integer> Pair(Integer maxCharacterCount) {
+        return null;
+    }
+
+    public static Pair<Character, Integer> Pair(char maxCharacter) {
+        return null;
     }
 }
