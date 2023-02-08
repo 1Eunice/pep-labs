858ce14e501e6f0f8877eb677679d396458957ee
1Eunice <1eunicegill@gmail.com>
2023-02-08 16:21:51 +0000

    Saved progress on MostCommonCharacter 2023-02-08 16:21:51 +0000

--- MostCommonCharacter/src/main/java/MostCommonCharacter.java
+++ MostCommonCharacter/src/main/java/MostCommonCharacter.java
@@ -11,49 +11,24 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        char chars [];
-        chars = str.toCharArray();
-
-        HashMap < Character, Integer> charCount = new HashMap<Character,Integer>();
-
-        char output = ' ';
-        for(char c : chars){
-            if(charCount.containsKey(c)){
-                charCount.put(c, charCount.get(c) + 1);
-            } else{
-                charCount.put(c, 1);
-            }
-        
-        }final Integer mostCommon = Collections.max(charCount.values());
-        for(Entry<Character, Integer> entry : charCount.entrySet()){
-            if(entry.getValue() == mostCommon){
-                output = (char) entry.getKey();
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>(); 
+         char mostCommonChar = ' ';
+        for (int i = 0; i < str.length(); i++){
+            if (map.containsKey(str.charAt(i))){
+                map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+              }       
+            else{
+                map.put(str.charAt(i), 1);
+            }           
+         }  
+        int maxValueInMap=(Collections.max(map.values()));  
+        for (Entry<Character, Integer> entry:map.entrySet()){  
+            if (entry.getValue()==maxValueInMap){
+                System.out.println(entry.getKey() + maxValueInMap);
+                mostCommonChar = entry.getKey();
             }
         }
-        return output;
-
-    }
-
+        return mostCommonChar;
+    } 
 }
 
-
-        //HashMap<Character, Integer> map = new HashMap<Character, Integer>(); 
-         //char maxappearchar = ' ';
-         //for (int i = 0; i < str.length(); i++){
-              //if (map.containsKey(str.charAt(i))){
-                //map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
-              //}       
-              //else{
-                //map.put(str.charAt(i), 1);
-            //}           
-         //}  
-            //int maxValueInMap=(Collections.max(map.values()));  
-            //for (Entry<Character, Integer> entry:map.entrySet()){  
-                //if (entry.getValue()==maxValueInMap){
-                    //System.out.println(entry.getKey() + maxValueInMap);
-                    //maxappearchar = entry.getKey();
-               // }
-            //}
-            //return maxappearchar;
-        //} 
-    //}
